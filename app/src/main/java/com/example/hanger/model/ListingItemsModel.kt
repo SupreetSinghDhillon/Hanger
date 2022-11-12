4b6a82fbda390dabe02c8b5ee2a0e53f7d345e5e
Carmen Law <carmenlaw08@gmail.com>
2022-11-11 17:08:26 -0800

    Added spinner to include category

--- app/src/main/java/com/example/hanger/model/ListingItemsModel.kt
+++ app/src/main/java/com/example/hanger/model/ListingItemsModel.kt
@@ -5,5 +5,6 @@ data class ListingItemsModel (
     var itemName: String? = "No name",
     var itemPrice: String? = "No price",
     var itemLocation: String? = "No Location",
-    var itemDesc: String? = "No description"
+    var itemDesc: String? = "No description",
+    var itemCategory: String? = "No category"
 )

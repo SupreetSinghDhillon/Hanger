5aa8e6421d59121533d02f319438cb0f9107b278
Patrick Zhao <patrick123cool@gmail.com>
2022-12-05 01:38:32 -0800

    Lag Optimizations
    No longer fetches every time a filter is applied

--- app/src/main/java/com/example/hanger/ListingsActivity.kt
+++ app/src/main/java/com/example/hanger/ListingsActivity.kt
@@ -59,6 +59,8 @@ class ListingsActivity: AppCompatActivity(), AdapterView.OnItemSelectedListener,
     private lateinit var textViewCategory: TextView
     private lateinit var recyclerViewListings: RecyclerView
     private lateinit var listings: ArrayList<ListingItemsModel>
+    private lateinit var fetchedListings: ArrayList<ListingItemsModel>
+    private lateinit var listingsAdapter: ListingAdapter
 //    private lateinit var dateOrderedListings: ArrayList<ListingItemsModel>
     private lateinit var db: DatabaseReference
 
@@ -91,7 +93,7 @@ class ListingsActivity: AppCompatActivity(), AdapterView.OnItemSelectedListener,
                 if (progress > 50) {
                     textViewDistance.text = "50+ km"
                 } else {
-                    textViewDistance.text = "$progress+ km"
+                    textViewDistance.text = "$progress km"
                 }
             }
 
@@ -101,7 +103,7 @@ class ListingsActivity: AppCompatActivity(), AdapterView.OnItemSelectedListener,
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
                 distanceFilter = seekBar!!.progress
-                fetchListings()
+                applyFilters()
             }
 
         })
@@ -111,6 +113,8 @@ class ListingsActivity: AppCompatActivity(), AdapterView.OnItemSelectedListener,
         recyclerViewListings.setHasFixedSize(true)
 
         listings = ArrayList()
+        fetchedListings = ArrayList()
+        listingsAdapter = ListingAdapter(listings, this)
         checkPermission()
     }
 
@@ -118,6 +122,7 @@ class ListingsActivity: AppCompatActivity(), AdapterView.OnItemSelectedListener,
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
 //        println("debug: selected spinner pos $pos")
         selectedFilter = parent?.getItemAtPosition(pos).toString()
+        applyFilters()
 //        println("debug: selected spinner text $selectedFilter")
     }
 
@@ -128,7 +133,7 @@ class ListingsActivity: AppCompatActivity(), AdapterView.OnItemSelectedListener,
 
     private fun fetchListings() {
         val category = intent.getIntExtra("CategoryId", 0)
-        println("debug: category is $category")
+//        println("debug: category is $category")
         db = Firebase.database.reference.child("Listings")
         val categoryQuery = db.orderByChild("itemCategory").equalTo(category.toDouble())
         val context = this
@@ -137,57 +142,12 @@ class ListingsActivity: AppCompatActivity(), AdapterView.OnItemSelectedListener,
 //                println("debug: snapshot is $snapshot")
 
                 listings.clear()
+                fetchedListings.clear()
                 for (data in snapshot.children) {
                     val listing = data.getValue<ListingItemsModel>()
-                    // https://stackoverflow.com/questions/43927273/android-how-to-convert-string-value-of-latlng-from-firebase-back-to-latlng
-                    var raw = listing!!.itemLatlng as String
-                    raw = raw.replace("[", "").replace("]", "")
-                    val latLngString = raw.split(",")
-                    println("debug: latLngString is $latLngString")
-                    val listingLatLng = LatLng(latLngString[0].toDouble(), latLngString[1].toDouble())
-                    val distance = SphericalUtil.computeDistanceBetween(currentLatLng, listingLatLng) / 1000 // km
-                    println("debug: distance is $distance")
-                    if (listing.isActive) {
-                        if (distanceFilter > 50 || distance < distanceFilter) {
-                            listings.add(listing)
-                        }
-                    }
+                    fetchedListings.add(listing!!)
                 }
-
-                // other filter sorting
-                if (selectedFilter == "Price: Low to High") {
-                    listings.sortWith(compareBy<ListingItemsModel> {
-                        it.itemPrice?.toDouble()
-                    })
-                } else if (selectedFilter == "Price: High to Low") {
-                    listings.sortWith(compareBy<ListingItemsModel> {
-                        it.itemPrice?.toDouble()
-                    })
-                    listings.reverse()
-                } else {
-                    listings.reverse()
-                }
-
-                val listingAdapter = ListingAdapter(listings, context)
-                recyclerViewListings.adapter = listingAdapter
-                listingAdapter.setOnItemClickListener(object : ListingAdapter.onItemClickListener {
-                    override fun onCardClicked(position: Int) {
-                        val intent = Intent(listingsContext, EditMyListingActivity::class.java)
-                        intent.putExtra("editing", false)
-                        intent.putExtra("userId", listings[position].userId)
-                        intent.putExtra("itemId", listings[position].itemId)
-                        intent.putExtra("itemName", listings[position].itemName)
-                        intent.putExtra("itemPrice", listings[position].itemPrice)
-                        intent.putExtra("itemLocation", listings[position].itemLocation)
-                        intent.putExtra("itemLatlng", listings[position].itemLatlng)
-                        intent.putExtra("itemCategory", listings[position].itemCategory)
-                        intent.putExtra("itemDesc", listings[position].itemDesc)
-                        intent.putExtra("itemActive", listings[position].isActive)
-                        startActivity(intent)
-                    }
-                })
-
-
+                applyFilters()
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -197,7 +157,60 @@ class ListingsActivity: AppCompatActivity(), AdapterView.OnItemSelectedListener,
 
     }
 
-    fun checkPermission() {
+    private fun applyFilters() {
+
+        var newListings: ArrayList<ListingItemsModel> = ArrayList()
+        for (listing in fetchedListings) {
+            var raw = listing.itemLatlng as String
+            raw = raw.replace("[", "").replace("]", "")
+            val latLngString = raw.split(",")
+//            println("debug: latLngString is $latLngString")
+            val listingLatLng = LatLng(latLngString[0].toDouble(), latLngString[1].toDouble())
+            val distance = SphericalUtil.computeDistanceBetween(currentLatLng, listingLatLng) / 1000 // km
+//            println("debug: distance is $distance")
+            if (listing.isActive) {
+                if (distanceFilter > 50 || distance < distanceFilter) {
+                    newListings.add(listing)
+                }
+            }
+        }
+        // other filter sorting
+        if (selectedFilter == "Price: Low to High") {
+            newListings.sortWith(compareBy<ListingItemsModel> {
+                it.itemPrice?.toDouble()
+            })
+        } else if (selectedFilter == "Price: High to Low") {
+            newListings.sortWith(compareBy<ListingItemsModel> {
+                it.itemPrice?.toDouble()
+            })
+            newListings.reverse()
+        } else {
+            newListings.reverse()
+        }
+
+        listings.clear()
+        listings.addAll(newListings)
+        listingsAdapter.notifyDataSetChanged()
+        recyclerViewListings.adapter = listingsAdapter
+        listingsAdapter.setOnItemClickListener(object : ListingAdapter.onItemClickListener {
+            override fun onCardClicked(position: Int) {
+                val intent = Intent(listingsContext, EditMyListingActivity::class.java)
+                intent.putExtra("editing", false)
+                intent.putExtra("userId", listings[position].userId)
+                intent.putExtra("itemId", listings[position].itemId)
+                intent.putExtra("itemName", listings[position].itemName)
+                intent.putExtra("itemPrice", listings[position].itemPrice)
+                intent.putExtra("itemLocation", listings[position].itemLocation)
+                intent.putExtra("itemLatlng", listings[position].itemLatlng)
+                intent.putExtra("itemCategory", listings[position].itemCategory)
+                intent.putExtra("itemDesc", listings[position].itemDesc)
+                intent.putExtra("itemActive", listings[position].isActive)
+                startActivity(intent)
+            }
+        })
+    }
+
+    private fun checkPermission() {
         if (Build.VERSION.SDK_INT < 23) return
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_CODE)
